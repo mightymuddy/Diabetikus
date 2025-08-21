68b0ffb54a7af7810992253ca05704278e0d36de
mightymuddy <84630238+mightymuddy@users.noreply.github.com>
2025-08-21 11:56:56 +0200

    Update changes

--- obj/Debug/net8.0-windows/Diabetikus.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/Diabetikus.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Diabetikus")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1ea6c921b6a5edb15ec34ceb43495fdf867b93c2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+250649f7e0ed76018e22fef536c33250a731669d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Diabetikus")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Diabetikus")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
