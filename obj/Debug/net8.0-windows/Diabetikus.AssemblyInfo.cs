3ac5ee10563f0a88cf3a378c3a3c043da7d26aff
mightymuddy <84630238+mightymuddy@users.noreply.github.com>
2025-08-21 12:04:21 +0200

    create docs file

--- obj/Debug/net8.0-windows/Diabetikus.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/Diabetikus.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Diabetikus")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+68b0ffb54a7af7810992253ca05704278e0d36de")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6d5d8865cd600bac5e8d3798b8b2bdc5b723d197")]
 [assembly: System.Reflection.AssemblyProductAttribute("Diabetikus")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Diabetikus")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
